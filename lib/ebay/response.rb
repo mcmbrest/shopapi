4b5057e0c7af3034456efd9f4dd04edad4eb0c10
unknown <MCM@.(none)>
2013-03-20 23:50:27 +0300

    commit

--- lib/ebay/response.rb
+++ lib/ebay/response.rb
@@ -1,9 +1,9 @@
 module ShopApi
   class Response
-    attr_accessor :response, :results
+    attr_accessor :response
     
     def initialize(json_response)
-      @response = json_response
+      @response = transform_json_response(json_response)
     end
     
     def success?
@@ -36,6 +36,29 @@ module ShopApi
       e = @response.response.select{|k,v| v.class == Hash && v.has_key?('Request') && v['Request'].has_key?('Errors')}
       e = e[e.keys[0]]['Request']['Errors']['Error']['Message'] if !e.blank?
     end     
+
+    protected
+    def transform_json_response(response)    
+      if response.class == Hash
+        r = Hash.new
+        response.keys.each do |k|
+          r[k] = transform_json_response(response[k])
+        end
+        return r
+      elsif response.class == Array 
+        if response.size == 1  
+          return transform_json_response(response[0])
+        else
+          r = Array.new
+          response.each do |a|
+            r.push(transform_json_response(a))
+          end
+          return r
+        end
+      else
+        return response
+      end
+    end
 
   end
 end
